69e0b3c758c50c3c593fa8a3e5678b9cfff69717
RIDDHIMA BHALLA <rid28bhalla@gmail.com>
2026-09-17 21:16:42 +0530

    init new repo

---
+++ src/main/java/com/paymentsd/plw/PlwApplication.java
@@ -0,0 +1,13 @@
+package com.paymentsd.plw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymentsd/plw/PlwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentsd.plw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
